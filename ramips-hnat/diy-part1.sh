dc0751e81625dbbf7d35b0b8aae0a039a060d329
White12352 <86940955+White12352@users.noreply.github.com>
2023-02-15 21:07:54 +0800

    Update diy-part1.sh

--- ramips-hnat/diy-part1.sh
+++ ramips-hnat/diy-part1.sh
@@ -21,5 +21,5 @@
 rm -f feeds.conf.default
 echo 'src-git packages https://github.com/immortalwrt/packages.git' >>feeds.conf.default
 echo 'src-git luci https://github.com/immortalwrt/luci.git' >>feeds.conf.default
-echo 'src-git routing https://github.com/openwrt/routing.git^18168b34' >>feeds.conf.default
-echo 'src-git telephony https://github.com/openwrt/telephony.git' >>feeds.conf.default
+echo 'src-git routing https://github.com/openwrt/routing.git^18168b3468' >>feeds.conf.default
+echo 'src-git telephony https://github.com/openwrt/telephony.git^b1bfd7d375' >>feeds.conf.default
